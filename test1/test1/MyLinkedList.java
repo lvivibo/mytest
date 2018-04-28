8a99a1a73e0aabdb2528409dd0c1cdcd39c3defc
bpli <bpli@BPLI-IN.cn.oracle.com>
2018-04-28 14:28:57 +0800

    fff

--- test1/test1/MyLinkedList.java
+++ test1/test1/MyLinkedList.java
@@ -1,23 +1,65 @@
 package test1;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class MyLinkedList {
-	LinkedNode head =new LinkedNode();
-	public MyLinkedList() {
-		head.next = null;
+	
+	Node head;
+	Node current;
+	
+	public void  add(int data){
+		if(null==head){
+			head = new Node(data);
+			current = head;
+		}else{
+			current.next = new Node(data);
+			current = current.next;
+		}
 	}
+	public void  print(){
+		current = head;
+		while(current!=null){
+			System.out.print(current.val+",");
+			current = current.next;
+		}
+	
+		
+	
+	}
+	class Node{
+		int val;
+		Node next;
+		public Node(int val) {
+			this.val = val;
+		}
+		
+		
+	}
+	///LinkedNode head =new LinkedNode();
+	/*public MyLinkedList() {
+		head.next = null;
+	}*/
 
 	
    
-	void add(LinkedNode node){
+	/*void add(LinkedNode node){
 		head.next = node;
-	}
+	}			
 	void list(){
 		
-	}
+	}	*/
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//List list1 = new LinkedList();
+		//list1.subList(fromIndex, toIndex);
+		MyLinkedList list = new MyLinkedList();
+		list.add(1);
+		/*list.add(2);
+		list.add(3);
+		list.add(4);*/
+		list.print();
+		
 	}
 
 }
