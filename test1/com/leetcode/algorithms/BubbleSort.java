bb0f3bb68325935a9cb1b41633aab1da1a521393
bpli <bpli@BPLI-IN.cn.oracle.com>
2018-06-07 16:11:19 +0800

    bubble sort

--- test1/com/leetcode/algorithms/BubbleSort.java
+++ test1/com/leetcode/algorithms/BubbleSort.java
@@ -20,7 +20,7 @@ public class BubbleSort {
 	}
 
 	public static void main(String[] args) {
-	 int[] a= {1,2,3,4,8,9,10};
+	 int[] a= {1,2,3,4,8,9,10 };
 	 BubbleSort.sort(a);
 	 for (int i = 0; i < a.length; i++) {
 		System.out.print(a[i]+",");
