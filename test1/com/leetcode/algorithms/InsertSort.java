5a902a608ff9df936357b0f9d5e25835318b9768
bpli <bpli@BPLI-IN.cn.oracle.com>
2018-06-07 16:39:13 +0800

    insert sort

---
+++ test1/com/leetcode/algorithms/InsertSort.java
@@ -0,0 +1,14 @@
+package com.leetcode.algorithms;
+
+public class InsertSort {
+	
+	void insertSort(int[] a) {
+		for (int i = 0; i < a.length; i++) {
+			if(b>a[i])
+		}
+	}
+	public static void main(String[] args) {
+		 int[] a= {1,4,9,10,3,2,8};
+	}
+
+}
